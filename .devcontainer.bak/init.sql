833e1e6f26f7512b7b2210612d620415e0577db8
Mischa van den Burg <mischavandenburg@gmail.com>
2025-04-11 15:48:38 +0000

    work: change dev container

---
+++ .devcontainer.bak/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE shelly_sensor_data (
+    timestamp TIMESTAMPTZ PRIMARY KEY,
+    device_id VARCHAR(50) NOT NULL,
+    temperature NUMERIC(5,2),
+    humidity NUMERIC(5,2),
+    battery INTEGER,
+    error INTEGER,
+);
+-- Create an index on the device_id for faster queries
+CREATE INDEX idx_device_id ON shelly_sensor_data(device_id);
+
+-- Add a comment to the table
+COMMENT ON TABLE shelly_sensor_data IS 'Stores sensor data from Shelly HT devices';
